5324905dec4e0df2cd860d8fd158ca6f18920f50
pgottipati <PGottipati@DCITAG8119>
2023-07-11 17:46:27 -0500

    Changs

--- Feature1StepDefinitions.cs
+++ Feature1StepDefinitions.cs
@@ -36,71 +36,7 @@ namespace TestProjectUI
 
             driver.Manage().Window.Maximize();
 
-            //driver.FindElement(By.XPath("//html/body/div[1]/div/header/div/div/div[2]/div[3]/div[1]/ul/li[3]/a")).Click();
-            //Thread.Sleep(1000);
-
-            //Thread.Sleep(1000);
-
-
-            //driver.FindElement(By.XPath("//html/body/div[1]/section[2]/div/div/header/div[1]/div/div[4]/section/div/form/div[2]/div/div/div[1]/input")).SendKeys("QA Automation Engineer");
-            //Thread.Sleep(1000);
-            //Thread.Sleep(1000);
-
-            //driver.FindElement(By.XPath("//html/body/div[1]/section[2]/div/div/header/div[1]/div/div[4]/section/div/form/div[2]/div/div/div[1]/input")).SendKeys(Keys.ArrowDown);
-            //Thread.Sleep(1000);
-            //Thread.Sleep(1000);
-            //driver.FindElement(By.XPath("//html/body/div[1]/section[2]/div/div/header/div[1]/div/div[4]/section/div/form/div[2]/div/div/div[1]/input")).SendKeys(Keys.Enter);
-
-            //Thread.Sleep(1000);
-
-            //Thread.Sleep(1000);
-
-
-            //string actualvalue = driver.FindElement(By.XPath("//html/body/div[2]/div[2]/div/div[1]/div[2]/section/div/div/div/div[1]/h1")).Text;
-            //Assert.IsTrue(actualvalue.Contains("Test Automation Engineer"), actualvalue + "Error");
-            //Thread.Sleep(1000);
-
-
-
-            //string actualvalue2 = driver.FindElement(By.XPath("//html/body/div[2]/div[2]/div/div[1]/div[2]/section/div/div/div/div[1]/section/div/div[1]/span[3]/span")).Text;
-            //Assert.IsTrue(actualvalue2.Contains("2322968"), actualvalue2 + "Error");
-
-
-            //string actualvalue3 = driver.FindElement(By.XPath("//html/body/div[2]/div[2]/div/div[2]/div/div/div[1]/section[1]/div/div/div[2]/div[2]/ul[1]/li[3]")).Text;
-            //Assert.IsTrue(actualvalue3.Contains("software"), actualvalue3 + "Error");
-
-
-            //string actualvalue4 = driver.FindElement(By.XPath("//html/body/div[2]/div[2]/div/div[2]/div/div/div[1]/section[1]/div/div/div[2]/div[2]/ul[1]/li[2]")).Text;
-            //Assert.IsTrue(actualvalue4.Contains("5+ years hands-on experience building test automation solutions for UI or service and/or transferrable software development experience"), actualvalue4 + "Error");
-
-
-            //string actualvalue5 = driver.FindElement(By.XPath("//html/body/div[2]/div[2]/div/div[2]/div/div/div[1]/section[1]/div/div/div[2]/div[2]/ul[1]/li[5]")).Text;
-            //Assert.IsTrue(actualvalue5.Contains("Cucumber/BDD"), actualvalue5 + "Error");
-
-
-            //Thread.Sleep(1000);
-
-            //Thread.Sleep(1000);
-
-            //driver.FindElement(By.XPath("//html/body/div[2]/div[2]/div/div[1]/div[2]/section/div/div/div/div[2]/div/a/ppc-content")).Click();
-
-            //Thread.Sleep(1000);
-            //Thread.Sleep(1000);
-
-
-            //var windowHandles = driver.WindowHandles.ToList();
-
-            //Console.WriteLine(windowHandles);
-
-            //driver.SwitchTo();
-            //Thread.Sleep(1000);
-            //Thread.Sleep(1000);
-
-            ///html/body/div[1]/div[2]/div/header/div[2]/div[3]/div/nav
-
-
-
-        }
+         }
 
 
         //I click on Careers
@@ -114,60 +50,6 @@ namespace TestProjectUI
 
             Thread.Sleep(1000);
 
-
-            //driver.FindElement(By.XPath("//html/body/div[1]/section[2]/div/div/header/div[1]/div/div[4]/section/div/form/div[2]/div/div/div[1]/input")).SendKeys("QA Automation Engineer");
-            //Thread.Sleep(1000);
-            //Thread.Sleep(1000);
-
-            //driver.FindElement(By.XPath("//html/body/div[1]/section[2]/div/div/header/div[1]/div/div[4]/section/div/form/div[2]/div/div/div[1]/input")).SendKeys(Keys.ArrowDown);
-            //Thread.Sleep(1000);
-            //Thread.Sleep(1000);
-            //driver.FindElement(By.XPath("//html/body/div[1]/section[2]/div/div/header/div[1]/div/div[4]/section/div/form/div[2]/div/div/div[1]/input")).SendKeys(Keys.Enter);
-
-            //Thread.Sleep(1000);
-
-            //Thread.Sleep(1000);
-
-
-            //string actualvalue = driver.FindElement(By.XPath("//html/body/div[2]/div[2]/div/div[1]/div[2]/section/div/div/div/div[1]/h1")).Text;
-            //Assert.IsTrue(actualvalue.Contains("Test Automation Engineer"), actualvalue + "Error");
-            //Thread.Sleep(1000);
-
-
-
-            //string actualvalue2 = driver.FindElement(By.XPath("//html/body/div[2]/div[2]/div/div[1]/div[2]/section/div/div/div/div[1]/section/div/div[1]/span[3]/span")).Text;
-            //Assert.IsTrue(actualvalue2.Contains("2322968"), actualvalue2 + "Error");
-
-
-            //string actualvalue3 = driver.FindElement(By.XPath("//html/body/div[2]/div[2]/div/div[2]/div/div/div[1]/section[1]/div/div/div[2]/div[2]/ul[1]/li[3]")).Text;
-            //Assert.IsTrue(actualvalue3.Contains("software"), actualvalue3 + "Error");
-
-
-            //string actualvalue4 = driver.FindElement(By.XPath("//html/body/div[2]/div[2]/div/div[2]/div/div/div[1]/section[1]/div/div/div[2]/div[2]/ul[1]/li[2]")).Text;
-            //Assert.IsTrue(actualvalue4.Contains("5+ years hands-on experience building test automation solutions for UI or service and/or transferrable software development experience"), actualvalue4 + "Error");
-
-
-            //string actualvalue5 = driver.FindElement(By.XPath("//html/body/div[2]/div[2]/div/div[2]/div/div/div[1]/section[1]/div/div/div[2]/div[2]/ul[1]/li[5]")).Text;
-            //Assert.IsTrue(actualvalue5.Contains("Cucumber/BDD"), actualvalue5 + "Error");
-
-
-            //Thread.Sleep(1000);
-
-            //Thread.Sleep(1000);
-
-            //driver.FindElement(By.XPath("//html/body/div[2]/div[2]/div/div[1]/div[2]/section/div/div/div/div[2]/div/a/ppc-content")).Click();
-
-            //Thread.Sleep(1000);
-            //Thread.Sleep(1000);
-
-
-            //var windowHandles = driver.WindowHandles.ToList();
-
-            //Console.WriteLine(windowHandles);
-
-            //driver.SwitchTo();
-            //Thread.Sleep(1000);
-            //Thread.Sleep(1000);
         }
 
 
